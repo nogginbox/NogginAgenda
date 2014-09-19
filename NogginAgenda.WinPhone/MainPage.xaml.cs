51d3b9023d3a2de318dc579180bd0a47e1dd8264
Richard Garside <richard.garside@nogginbox.co.uk>
2014-09-19 11:15:59 +0100

    Added Windows Phone 8 Project.

---
+++ NogginAgenda.WinPhone/MainPage.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Phone.Controls;
+using Xamarin.Forms;
+
+namespace NogginAgenda.WinPhone
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            Forms.Init();
+            Content = NogginAgenda.App.GetMainPage().ConvertPageToUIElement(this);
+        }
+    }
+}
