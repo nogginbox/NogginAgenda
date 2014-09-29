7f95058aba62e8549049ad6fc2e8488bd3904e41
Richard Garside <richard.garside@nogginbox.co.uk>
2014-09-29 15:41:16 +0100

    Now caching images for longer. Needed to add Uri Value Converter to help make this work.

---
+++ NogginAgenda/Xaml/Converters/UriConverter.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Xamarin.Forms;
+using System.Globalization;
+
+namespace NogginAgenda.Xaml.Converters
+{
+
+    public class UriConverter: IValueConverter   
+    {
+          
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            var uriString = value as String;
+            if (string.IsNullOrEmpty (uriString)) return null;
+
+            return new Uri (uriString);
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException("Not implemented url -> string conversion"); 
+        }
+    }
+}
